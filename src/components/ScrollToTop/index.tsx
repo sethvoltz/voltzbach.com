bad83c8ecc57eaacf4fcc081971079655bf682d9
Seth Voltz <seth@designgods.net>
2017-05-27 21:36:07 -0700

    Change type to <any> for now due to something in TS update

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -4,7 +4,7 @@ import { RouteComponentProps, withRouter } from "react-router";
 // From: https://gist.github.com/vivmaha/f34fdea72d0f1fa0467a671b5ec7321e
 // Scroll restoration based on https://reacttraining.com/react-router/web/guides/scroll-restoration.
 export var ScrollToTop = withRouter(
-  class ScrollToTopWithoutRouter extends React.Component<RouteComponentProps<any>, void> {
+  class ScrollToTopWithoutRouter extends React.Component<any, void> {
     componentDidUpdate(prevProps: Readonly<RouteComponentProps<any>>) {
       if (this.props.location !== prevProps.location) {
         window.scrollTo(0, 0)
